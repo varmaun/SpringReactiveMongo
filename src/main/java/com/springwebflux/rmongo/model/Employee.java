fafc789759d9a8261057f8985b6b9635e2f30e52
varmaun9 <varma.179@gmail.com>
2020-08-18 10:48:27 +0100

    Changes in model class  - annotated with @Data and other lombok impl to reduce boiler plate code.

--- src/main/java/com/springwebflux/rmongo/model/Employee.java
+++ src/main/java/com/springwebflux/rmongo/model/Employee.java
@@ -1,47 +1,26 @@
 package com.springwebflux.rmongo.model;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Scope(scopeName = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
-@Document
-public class Employee {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Document("employee")
+public class Employee{
 	@Id
-	int id;
-	String name;
-	long salary;
-
-	// Getters and setters
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public long getSalary() {
-		return salary;
-	}
-
-	public void setSalary(long salary) {
-		this.salary = salary;
-	}
-	
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
-	}
+	@JsonProperty
+	private Integer id;
+	@JsonProperty
+	private String name;
+	@JsonProperty
+	private int salary;
 }
